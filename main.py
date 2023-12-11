0bd91da9f0ffb69d6dd9f6378b5473671b27e2a0
Alaminosman <152073707+Alaminosman@users.noreply.github.com>
2023-12-11 14:49:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Ahoy World, you need this No. 1234567",
+                     from_='+12016139872',
+                     to= os.environ["CRAIGS_NUMBER"]
+                 )
+
+print(message.sid)
